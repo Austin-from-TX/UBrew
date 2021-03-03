84ceb4c11a1381201f2685fae494675cbcd53f7e
Austin <austin.smithfield@gmail.com>
2021-03-03 02:37:14 +0000

    added base font style

--- react-app/src/components/auth/LoginForm.js
+++ react-app/src/components/auth/LoginForm.js
@@ -37,7 +37,7 @@ const LoginForm = ({ authenticated, setAuthenticated }) => {
         ))}
       </div>
       <div>
-        <label htmlFor="email">Email</label>
+        <label style={{fontFamily: 'Bourbon Grotesque'}} htmlFor="email">Email</label>
         <input
           name="email"
           type="text"
